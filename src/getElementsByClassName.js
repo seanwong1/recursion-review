9d1b5e5bb5959a2bc27ef5cab65b35b71c444459
Justin Cheng <jucheng.ca@gmail.com>
2022-10-27 20:43:11 -0700

    Complete Bare Minimum Requirements

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,43 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+
+  var result = [];
+
+  var innerFunction = function(element) {
+    console.log(element);
+    if (element.classList !== undefined) {
+      if (element.classList.contains(className)) {
+        result.push(element);
+      }
+    }
+    for (var i = 0; i < element.childNodes.length; i++) {
+      innerFunction(element.childNodes[i]);
+    }
+  };
+  innerFunction(document.body);
+  return result;
+  // base case if node includes classname
+  //console.log(element.classList);
+  /*if (element.classList !== undefined) {
+    if (element.classList.contains(className)) {
+      console.log('test');
+      result.push(element);
+      return result;
+      // getElementsByClassName(className, element)
+      // if (element.childNodes.length === 0) {}
+    }
+  } else {
+    if (element.classList.contains(className)) {
+      result.push(element);
+    }
+    for (var i = 0; i < element.childNodes.length; i++) {
+      //console.log(element.childNodes[i]);
+      // getElementsByClassName(className)
+      result.push(getElementsByClassName(className, element.childNodes[i]));
+    }
+  }
+  console.log(result);
+  return result;*/
 };
