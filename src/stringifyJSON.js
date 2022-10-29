18ecaab90caab97acd48411a740b793234d253e0
Sean Wong <sean.uuong@gmail.com>
2022-10-29 12:58:06 -0700

    Complete stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,54 @@
 
 // but you don't so you're going to write it from scratch:
 
+/*
+unstringifiableValues = [
+  {
+    'functions': function() {},
+    'undefined': undefined
+  }
+];
+*/
+
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (obj === null) {
+    return 'null';
+  } else if (obj === undefined) {
+    return null;
+  } else if (typeof(obj) === 'string') {
+    return '"' + obj + '"';
+  } else if (typeof(obj) === 'number') {
+    return obj.toString();
+  } else if (typeof(obj) === 'boolean') {
+    return obj.toString();
+  } else if (Array.isArray(obj) === true) {
+    if (obj.length === 0) {
+      return '[]';
+    } else {
+      return '[' + _.map(obj, stringifyJSON) + ']';
+    }
+  } else if (typeof(obj) === 'object' && Array.isArray(obj) === false) {
+    if (Object.keys(obj).length === 0) {
+      return '{}';
+    } else {
+      var deb = '';
+      _.each(obj, function(item, key) {
+        if (typeof item === 'function') {
+          delete obj[key];
+        } else if (item === undefined) {
+          delete obj[key];
+        } else {
+          deb += stringifyJSON(key) + ':' + stringifyJSON(item);
+          if (Object.keys(obj).length > 1) {
+            deb += ',';
+          }
+        }
+      });
+      if (deb[deb.length - 1] === ',') {
+        deb = deb.slice(0, -1);
+      }
+      return '{' + deb + '}';
+    }
+  }
 };
+
